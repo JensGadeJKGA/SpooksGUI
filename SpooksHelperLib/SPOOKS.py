efd062817d695ef1fcf7a8f2b7ce741f958a6c21
JensGadeJKGA <jkga@cowi.com>
2025-05-16 09:37:12 +0200

    cleanup

--- SpooksHelperLib/SPOOKS.py
+++ SpooksHelperLib/SPOOKS.py
@@ -42,95 +42,57 @@ class spooksfile():
         
 
     def GenerateSPOOKSInputFile(self, Analysis):
-
-        Project = Analysis.get('Project')
-        Initials = Analysis.get('Initials')
-        Checker = Analysis.get('Checker')
-        Approver = Analysis.get('Approver')
-        Subject = Analysis.get('Subject')
-        SlopeFront = Analysis.get('SlopeFront')
-        SlopeBack = Analysis.get('SlopeBack')
-        LoadFront = Analysis.get('DesignLoadFront')
-        LoadBack = Analysis.get('DesignLoadBack')
-        WaterLevelBack = Analysis.get('WaterLevelBack')
-        WaterLevelFront = Analysis.get('WaterLevelFront')
-        WaterDensity = Analysis.get('WaterDensity')
         State = Analysis.get('State')
-        DesignSoilLayersFront = Analysis.get('DesignSoilLayersFront')
-        DesignSoilLayersBack = Analysis.get('DesignSoilLayersBack')
-        iA = Analysis.get('iA')
-        iB = Analysis.get('iB')
-        iC = Analysis.get('iC')
-        zT = Analysis.get('zT')
-        zR = Analysis.get('LevelLoadBack')
-        AnchorLevel = Analysis.get('AnchorLevel')
-        PrescrbAnchorForce = Analysis.get('PrescrbAnchorForce')
-        zB = Analysis.get('zB')
-        WD = Analysis.get('WD')
-        CC = Analysis.get('CC')
-        AddPressureProfile = Analysis.get('AddPressureProfile')
-        AddPress_z = Analysis.get('AddPress_z')
-        AddPress_ez = Analysis.get('DesignAddPress_ez')
-        
-        Geninf = [format(SlopeFront,'.2f'), 
-                format(SlopeBack,'.2f'),
-                format(LoadFront,'.2f'), 
-                format(LoadBack,'.2f'), 
-                format(WaterLevelFront,'.2f'), 
-                format(WaterLevelBack,'.2f'), 
-                format(WaterDensity,'.2f')]
-                
-        
-        Generalinfo = ""
-        
-        for item in Geninf: # creates the right amount of space between columns
-            if len(str(item)) == 4:
-                space = '      ' # 6 spaces
-            if len(str(item)) == 5:
-                space = '     '  # 5 spaces
-            if len(str(item)) == 6:
-                space = '    '   # 4 spaces
-            Generalinfo += space + str(item)
-        
+
+        Geninf = [format(Analysis.get('SlopeFront'),'.2f'), 
+                format(Analysis.get('SlopeBack'),'.2f'),
+                format(Analysis.get('DesignLoadFront'),'.2f'), 
+                format(Analysis.get('DesignLoadBack'),'.2f'), 
+                format(Analysis.get('WaterLevelFront'),'.2f'), 
+                format(Analysis.get('WaterLevelBack'),'.2f'), 
+                format(Analysis.get('WaterDensity'),'.2f')]
+    
+        Generalinfo = utils.AddSpaces(Geninf)
         ############ First lines in SPOOKSWAT input file (L)
         
-        L = ['<', 'Project:' +' '+ Project, 'Initials:' +' '+ Initials, 'Subject:' +' '+str(Subject), '>', '<', Generalinfo, '>', '<']
+        L = ['<', 'Project:' +' '+ Analysis.get('Project'), 'Initials:' +' '+ Analysis.get('Initials'), 'Subject:' +' '+str(Analysis.get('Subject')), '>', '<', Generalinfo, '>', '<']
         
         
         
         ################# SOIL FRONT ####################################
 
-        L=soilprofiles.designsoillayer(DesignSoilLayersFront, State, L)
+        L=soilprofiles.designsoillayer(Analysis.get('DesignSoilLayersFront'), State, L)
     
         ########################## SOIL BACK ##############################
 
-        L=soilprofiles.designsoillayer(DesignSoilLayersBack, State, L)
+        L=soilprofiles.designsoillayer(Analysis.get('DesignSoilLayersBack'), State, L)
         
         ################## FAILURE MODE (ANCHOR COEFFICIENTS) ###################
         AnchCoeffVars = {
-            "iA": iA,
-            "iB": iB,
-            "iC": iC,
-            "zT": zT,
-            "zR": zR
+            "iA": Analysis.get('iA'),
+            "iB": Analysis.get('iB'),
+            "iC": Analysis.get('iC'),
+            "zT": Analysis.get('zT'),
+            "zR": Analysis.get('LevelLoadBack')
         }
-        Coeff_temp = self.anchorLevel(AnchorLevel, PrescrbAnchorForce, AnchCoeffVars)
+        Coeff_temp = self.anchorLevel(Analysis.get('AnchorLevel'), Analysis.get('PrescrbAnchorForce'), AnchCoeffVars)
         Coeff = utils.AddSpaces(Coeff_temp)
         L.append(Coeff)
-        if AnchorLevel == None:
+        if Analysis.get('AnchorLevel') == None:
             L.append('                    ')
         
         ############ KING POST WALL PARAMETERS (if data is properly entered in excel input file)
-            
+        zB = Analysis.get('zB')
+        WD = Analysis.get('WD')
+        CC = Analysis.get('CC')
+
         if zB != None and WD != None and CC != None:
-            
-            KingPost_temp = [format(zB,'.2f'),
-                            format(WD,'.2f'),
-                            format(CC,'.2f')]
             
             KingPostParam = ""
 
-            KingPostParam = utils.AddSpaces(KingPost_temp)
+            KingPostParam = utils.AddSpaces([format(zB,'.2f'),
+                            format(WD,'.2f'),
+                            format(CC,'.2f')])
             L.append('                                        ' + KingPostParam) #+40 init spaces
         
         elif zB == None and WD == None and CC == None:
@@ -154,18 +116,18 @@ class spooksfile():
         
         ############### ADDITIONAL PRESSURE FILE #########################
         
-        if AddPressureProfile in ['AP1', 'AP2', 'AP3', 'AP4', 'AP5', 'AP6', 'AP7', 'AP8', 'AP9', 'AP10']:
+        if Analysis.get('AddPressureProfile') in ['AP1', 'AP2', 'AP3', 'AP4', 'AP5', 'AP6', 'AP7', 'AP8', 'AP9', 'AP10']:
         
             ## Additional pressure file
             addpressfile = []
         
             
-            APlevel = utils.AddSpaces(AddPress_z)
+            APlevel = utils.AddSpaces(Analysis.get('AddPress_z'))
             
             addpressfile.append(APlevel+'    ')
             addpressfile.append('  ')
             
-            APvalue = utils.AddSpaces(AddPress_ez)
+            APvalue = utils.AddSpaces(Analysis.get('AddPress_ez'))
             
             addpressfile.append(APvalue+'    ')
             
